b770805f788fb545088215a25edd7e77ff3054bd
selenoid <selenoid@gmailcom>
2026-02-10 03:16:49 +0300

    interim commit

--- app/(tabs)/index copy.tsx
+++ app/(tabs)/index copy.tsx
@@ -1,23 +1,21 @@
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
-import { ImageSourcePropType, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Button from '../components/Button';
 import CircleButton from '../components/CircleButton';
-import EmojiList from '../components/EmojiList';
-import EmojiPicker from '../components/EmojiPicker';
-import EmojiSticker from '../components/EmojiSticker';
 import IconButton from '../components/IconButton';
 import ImageViewer from '../components/ImageViewer';
+
+import EmojiPicker from '../components/EmojiPicker';
+
 
 const PlaceholderImage = require('../assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
-
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(true);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -54,7 +52,6 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -71,7 +68,7 @@ export default function Index() {
         </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+        {/* Emoji list component will go here */}
       </EmojiPicker>
     </View>
   );
---
+++ app/components/EmojiSticker.tsx
@@ -0,0 +1,16 @@
+import { Image } from 'expo-image';
+import { ImageSourcePropType, View } from 'react-native';
+
+type Props = {
+  imageSize: number;
+  stickerSource: ImageSourcePropType;
+};
+
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  console.log('LAGARI!..', imageSize, stickerSource)
+  return (
+    <View style={{ top: -350 }}>
+      <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
+    </View>
+  );
+}
