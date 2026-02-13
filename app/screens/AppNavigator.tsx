02139e0faa9e1fd2e550cb131c96d5dce595d740
selenoid <selenoid@gmailcom>
2026-02-13 21:15:26 +0300

    kabalang

--- app/screens/AppNavigator.tsx
+++ app/screens/AppNavigator.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AccountScreen from '../navigation/AccountScreen';
 import HomeScreen from './HomeScreen';
 
@@ -7,17 +8,20 @@ const Tab = createBottomTabNavigator();
 
 export default function AppNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Accounts" component={AccountScreen} />
-    </Tab.Navigator>
+    <SafeAreaProvider>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Accounts" component={AccountScreen} />
+      </Tab.Navigator>
+    </SafeAreaProvider>
+
   );
 }
 
 
 /* const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,r
     backgroundColor: '#25292e',
     justifyContent: 'center',
     alignItems: 'center',
